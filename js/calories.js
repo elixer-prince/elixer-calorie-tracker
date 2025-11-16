7ddf55fa156a4f8779bd878ea25d2e8ffea79085
Daniel Leith <elixerprince@gmail.com>
2025-11-16 12:03:49 -0500

    Change calorie input colour based on calorie limit

--- js/calories.js
+++ js/calories.js
@@ -12,8 +12,18 @@ const resetCalorieButton = document.querySelector("#calorie-reset-btn");
 const cancelUpdateButton = document.querySelector("#cancel-update-btn");
 
 /*=================================================
+                    VALUES
+=================================================*/
+
+const caloriesAlreadyConsumed =
+    JSON.parse(localStorage.getItem("totalCaloriesConsumedToday")) || 0;
+const calorieDailyLimit = 100;
+
+/*=================================================
                 INITIALISATION
 =================================================*/
+
+updateCalorieDisplayColour(caloriesAlreadyConsumed, calorieDailyLimit);
 
 totalCaloriesDisplay.textContent = JSON.parse(
     localStorage.getItem("totalCaloriesConsumedToday")
@@ -57,13 +67,28 @@ function resetCalorieTotal() {
     if (caloriesAlreadyConsumed <= 0) return alert("You ate nothing yet!");
 
     if (confirm("Are you sure you want to reset your total calories?")) {
+        updateCalorieDisplayColour(0, calorieDailyLimit);
         totalCaloriesDisplay.textContent = String(0);
         localStorage.setItem("totalCaloriesConsumedToday", JSON.stringify(0));
     }
 }
 
+function updateCalorieDisplayColour(
+    caloriesAlreadyConsumed,
+    calorieDailyLimit
+) {
+    if (caloriesAlreadyConsumed < calorieDailyLimit) {
+        totalCaloriesDisplay.classList.add("deficit");
+        totalCaloriesDisplay.classList.remove("sufficient");
+        return;
+    }
+
+    totalCaloriesDisplay.classList.add("sufficient");
+    totalCaloriesDisplay.classList.remove("deficit");
+}
+
 function updateCalorieTotal() {
-    const caloriesAlreadyConsumed = Number(totalCaloriesDisplay.textContent);
+    let caloriesAlreadyConsumed = Number(totalCaloriesDisplay.textContent);
     const caloriesToAdd = Number(calorieInput.value);
     const totalCaloriesConsumedToday = caloriesAlreadyConsumed + caloriesToAdd;
 
@@ -71,6 +96,9 @@ function updateCalorieTotal() {
         return alert("Input calories must be greater than zero!");
 
     totalCaloriesDisplay.textContent = String(totalCaloriesConsumedToday);
+    caloriesAlreadyConsumed = Number(totalCaloriesDisplay.textContent);
+
+    updateCalorieDisplayColour(caloriesAlreadyConsumed, calorieDailyLimit);
     calorieInput.value = null;
     localStorage.setItem(
         "totalCaloriesConsumedToday",
