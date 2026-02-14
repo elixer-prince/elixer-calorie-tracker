5f1170e243b9fd936c4f0e9eda03fd58b07c3447
Daniel Leith <elixerprince@gmail.com>
2026-02-14 00:36:03 -0500

    chore(ts): create a helper function for fetching elements without ts screaming

---
+++ src/ts/helpers/utils.ts
@@ -0,0 +1,7 @@
+export function getElementOrThrow<T extends Element>(selector: string): T {
+  const el = document.querySelector<T>(selector);
+  if (!el) {
+    throw new Error(`Element not found: ${selector}`);
+  }
+  return el;
+}
